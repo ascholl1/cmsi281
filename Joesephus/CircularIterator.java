ca9cabc0b92a13ea366722bee141344aa7b24873
Alyssa Scholl <ascholl1@lion.lmu.edu>
2016-11-08 00:20:57 -0800

    just tracking these

---
+++ Joesephus/CircularIterator.java
@@ -0,0 +1,15 @@
+import java.util.Iterator;
+
+public interface CircularIterator extends Iterator {
+
+    // Removes the kth element in the list
+    // and returns the string value of that element
+    public String removeKthElement(int k);
+
+    // Only one element is left in the collection
+    public boolean oneElementLeft();
+
+    //added removeString method because it wanted there to be a remove method without any arguments
+    //public void removeString(String s);
+
+}
