e9eb0429584600323e82b8c78dcd89a7d8fe8826
Alyssa Scholl <ascholl1@lion.lmu.edu>
2016-11-08 09:42:47 -0800

    added a test

--- Joesephus/JosephusSolver.java
+++ Joesephus/JosephusSolver.java
@@ -4,6 +4,7 @@ public class JosephusSolver {
 
   public static void main(String[] args) {
     int k1 = 5;
+    int k2 = 3;
     String [] test1 = { "Fido", "Delilah", "Rugrat", "Joshua", "Sid", 
                         "Bow-wow", "Bogo", "Pogo", "Mimi", "Chloe" };
     CircularArrayList arrayList = new CircularArrayList(test1);
@@ -17,5 +18,19 @@ public class JosephusSolver {
       i.removeKthElement(k1);
     }
     assert linkedList.first() == "Rugrat";
-  }
+
+    String [] test2 = { "element1", "element2", "element3", "element4", "element5", 
+                        "element6", "element7", "element8" };
+    CircularArrayList arrayList2 = new CircularArrayList(test2);
+    for (CircularIterator i = arrayList2.iterator(); !i.oneElementLeft(); ) {
+      i.removeKthElement(k2);
+    }
+    assert arrayList.first() == "element7";
+
+    CircularLinkedList linkedList2 = new CircularLinkedList(test2);
+        for (CircularIterator i = linkedList2.iterator(); !i.oneElementLeft(); ) {
+            i.removeKthElement(k2);
+        }
+        assert linkedList.first() == "element7";
+    }
 }
